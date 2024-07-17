96803cd5c62c686c6885466e50f5ee91033f004e
Feizai1123 <3091146630@qq.com>
2024-07-17 17:50:09 +0800

    build:构建项目

---
+++ src/main/java/com/wangjunblog/WangjunBlogApplication.java
@@ -0,0 +1,13 @@
+package com.wangjunblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WangjunBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangjunBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wangjunblog/WangjunBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wangjunblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangjunBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
